cfffcb02d61c63ace2d2a4ebd525cff11d32793a
BALAJI <wbalaji2021@gmail.com>
2023-04-29 23:19:12 +0530

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="#"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                    </a>
+
+                    <p>
+                        Built by |
+                        <a
+                            href="#"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Balaji 
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
